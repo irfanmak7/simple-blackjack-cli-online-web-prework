fb812b2a9e6577608130cf01c59b1825a5f5c289
Irfan Maknojia <irfan.mak7@gmail.com>
2018-11-08 02:00:52 +0000

    automatically backed up by learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -35,8 +35,18 @@ def initial_round
   total
 end
 
-def hit?
+def hit?(card_total)
   # code hit? here
+  prompt_user
+  user_input = get_user_input
+  if user_input == 'h'
+    card_total += deal_card
+  elsif user_input == 's'
+
+  else
+    invalid_command
+    prompt_user
+
 end
 
 def invalid_command
